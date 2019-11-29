584317151a0c6aad2b8bcf3722d281b9a1fcfb47
Clemens Kaserer <clemens.kaserer@gmail.com>
2019-11-29 09:22:29 +0100

    I had a cup of tea and now it's fixed

--- bootstrap/commons-wrapper.sh
+++ bootstrap/commons-wrapper.sh
@@ -6,6 +6,6 @@ ABOVE_SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" > /dev/null && cd .. &
 docker run --rm -it \
   -v ${ABOVE_SCRIPT_DIR}:/home/hogaramaInfra \
   quay.io/openshift/origin-cli:latest /home/hogaramaInfra/bootstrap/commons.sh \
-    --oc-admin-token "dDrZg1tvISPMaJekho7buCfL9aJBgD5n99Jm62l-tDo" \
-    --oc-cluster https://api.learningfriday.aws.openshift.gepardec.com:6443 \
+    --oc-admin-token "$(oc whoami -t)" \
+    --oc-cluster https://api.p.aws.ocp.gepardec.com:6443 \
     --namespace commons
