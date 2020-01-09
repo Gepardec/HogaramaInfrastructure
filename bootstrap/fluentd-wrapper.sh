0f74715d27a9b710eb1af4c9e0ead15e7a277c46
fhochleitner <felix.hochleitner@gepardec.com>
2020-01-09 09:04:17 +0100

    changed default namespace for fluentd to hogarama

--- bootstrap/fluentd-wrapper.sh
+++ bootstrap/fluentd-wrapper.sh
@@ -8,6 +8,6 @@ docker run --rm -it \
   quay.io/openshift/origin-cli:latest /home/hogaramaInfra/bootstrap/fluentd.sh \
     --oc-admin-token "$(oc whoami -t)" \
     --oc-cluster https://api.p.aws.ocp.gepardec.com:6443 \
-    --namespace hogarama \
+    --namespace hogajama-test \
     --git-branch $(git branch | grep \* | cut -d ' ' -f2) \
     --force
